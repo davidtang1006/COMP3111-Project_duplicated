95a408b02ab594f1b43bb59ed029c47567469cf1
David Tang <David Tang@TABLET-G37VKJG>
2018-10-09 19:47:35 +0800

    Made some tiny changes

--- src/main/java/comp3111/webscraper/Controller.java
+++ src/main/java/comp3111/webscraper/Controller.java
@@ -69,8 +69,8 @@ public class Controller {
     		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
     	}
     	textAreaConsole.setText(output);
-    	
-    	
+    	// Add the following line
+    	labelCount.setText("Hi");
     }
     
     /**
